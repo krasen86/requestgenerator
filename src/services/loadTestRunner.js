7cd0a219b47bbbb17401526018579c3abe430517
Krasen Parvanov <krasenparvanov@gmail.com>
2020-12-11 12:12:55 +0100

    #1 Refactor load test runnuner and add method call to compare

--- src/services/loadTestRunner.js
+++ src/services/loadTestRunner.js
@@ -2,14 +2,16 @@ const {BookingGenerator} = require( "./bookingGenerator");
 const {Publisher} = require( "./publisher");
 const {Subscriber} = require("./subscriber");
 let storage = require('./requestResponseStorage');
+const {RequestResponseComparator} = require("./requestResponseComparator");
 
 class LoadTestRunner {
     constructor() {
     }
 
-    async startStressTest(message) {
+    async startLoadTest(message) {
         let availability = JSON.parse(message);
         let bookingDateAndTime = [];
+        let numberOfRequestsToSend = 100;
         for (let i = 0; i < availability.length; i++) {
             let date = Object.keys(availability[i])[0];
             for (let j = 0; j < availability[i][date].length; j++) {
@@ -21,17 +23,22 @@ class LoadTestRunner {
         subscriber.topicUnSubscriber("availability/1");
         let booking = new BookingGenerator();
         let publisher = new Publisher();
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < numberOfRequestsToSend; i++) {
             setTimeout(function(){
                 //console.log(i);
                 let request = booking.createRequest(bookingDateAndTime[i].date, bookingDateAndTime[i].timeSlot);
                 let requests = storage.requests;
-                requests.push(request);
                 publisher.publishToBroker(request);
+                request.sendAt = Date.now();
+                requests.push(request);
+                if (i === numberOfRequestsToSend) {
+                    let comparator = new RequestResponseComparator();
+                    comparator.compare();
+                }
             }, i*100);
+
         }
     }
-
 
 }
 
