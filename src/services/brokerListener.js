9f1f9bb60844a9d874bdb952746b70ec1afdf177
Krasen Parvanov <krasenparvanov@gmail.com>
2020-12-11 12:10:22 +0100

    #1 Add time stamp for incoming responses

--- src/services/brokerListener.js
+++ src/services/brokerListener.js
@@ -9,16 +9,14 @@ class BrokerListener {
         MQTT.on('message', function (topic, message) {
             if (topic === "availability/1") {
                 let stressTestRunner = new LoadTestRunner();
-                console.log("Called")
-                stressTestRunner.startStressTest(message);
+                stressTestRunner.startLoadTest(message);
             }
             else if (topic.includes("response")){
                 const buffer = message.toString('utf-8');
                 let response = JSON.parse(buffer);
                 let responses = storage.responses;
+                response.receivedAt = Date.now();
                 responses.push(response);
-                //console.log(responses)
-
             }
         })
     }
